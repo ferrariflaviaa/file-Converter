5a034e5f41c677a7b9e8b50c5f8afdcbb04b30db
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-10-04 14:09:22 -0300

    feat: finished

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 let Reader = require("./Reader")
+let Processor = require("./Processor")
+let Table = require("./Table")
+let HtmlParser = require("./HtmlParser")
+let Writer = require("./Writer")
+let PdfWriter = require("./PdfWriter")
 let leitor = new Reader();
+let escritor = new Writer();
+
 
 
 async function main() {
   let dados = await leitor.Read("./user.csv")
-  console.log(dados)
+  let dadosProcessados = Processor.Process(dados)
+  let tabela = new Table(dadosProcessados)
+  let html = await HtmlParser.Parse(tabela)
+
+
+  escritor.Write(Date.now() + ".html", html)
+  PdfWriter.WriterPDF(Date.now + ".PDF", html)
 }
 
 main();
